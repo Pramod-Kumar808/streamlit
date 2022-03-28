f5d98b23e7b83e21317cc3a5c2feff8584899530
Pramod Kumar N <Pramod_Nagaraj@dietsmann.com>
2022-03-28 16:01:07 +0200

    done

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import shutil
 import PIL
 from PIL import Image
 
-image = st.upload_file("Upload an image")
-if None not in (image):
+image = st.file_uploader("Upload an image")
+if image is not None:
     os.makedirs("temp", exist_ok=True)
     picture = Image.open(image)
     picture = picture.save(f'temp/{image.name}')
     image_file = f"temp/{image.name}" 
     data_image = Image.open(image_file)
     st.image(data_image)
+else:
+    st.warning("Please upload a file for test")
 
 
 
