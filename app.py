75be4bed640065c70aec4a7d0757fa1fececc50f
Pramod Kumar N <Pramod_Nagaraj@dietsmann.com>
2022-03-28 16:09:05 +0200

    change

--- app.py
+++ app.py
@@ -8,10 +8,20 @@ image = st.file_uploader("Upload an image")
 if image is not None:
     os.makedirs("temp", exist_ok=True)
     picture = Image.open(image)
-    picture = picture.save(f'temp/{image.name}')
-    image_file = f"temp/{image.name}" 
-    data_image = Image.open(image_file)
-    st.image(data_image)
+
+    width, height = picture.size
+    left = 5
+    top = height / 4
+    right = 164
+    bottom = 3 * height / 4
+
+    im1 = picture.crop((left, top, right, bottom))
+    picture = picture.save(f"temp/{im1}")
+
+    # image_file = f"temp/{im1}" 
+    # data_image = Image.open(image_file)
+    # st.image(data_image)
+    
 else:
     st.warning("Please upload a file for test")
 
