5ce67c29290cc826f0eec749ec5d5aeb6ee5ea58
Pramod Kumar N <Pramod_Nagaraj@dietsmann.com>
2022-03-28 16:37:41 +0200

    done

--- app.py
+++ app.py
@@ -21,26 +21,7 @@ if image is not None:
 
     for i in os.listdir("temp"):
         st.write(i)
-
-    # image_file = f"temp/{im1}" 
-    # data_image = Image.open(im1)
-    # picture = data_image.save(f"temp/{data_image.name}")
     
 else:
     st.warning("Please upload a file for test")
-
-# st.session_state['answer'] = ''
-
-# st.write(st.session_state)
-
-# realans = ['', 'abc', 'edf']
-
-# if  st.session_state['answer'] in realans:
-#     answerStat = "correct"
-# elif st.session_state['answer'] not in realans:
-#     answerStat = "incorrect"
-
-# st.write(st.session_state)
-# st.write(answerStat)
-
 
