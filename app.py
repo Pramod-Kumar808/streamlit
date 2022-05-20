e98cf0f1d93172ff7b1ad5b05d7ee3566183447b
Pramod Kumar N <Pramod_Nagaraj@dietsmann.com>
2022-05-20 13:16:14 +0200

    ready

--- app.py
+++ app.py
@@ -4,23 +4,12 @@ import shutil
 import PIL
 import random
 from PIL import Image
+import cv2
 
 image = st.file_uploader("Upload an image")
 if image is not None:
-    os.makedirs("temp", exist_ok=True)
-    picture = Image.open(image)
-    width, height = picture.size
-    left = 155
-    top = 65
-    right = 360
-    bottom = 270
-
-    im1 = picture.crop((left, top, right, bottom))
-    save_name = random.random()
-    im1.save(f"temp/{save_name}.jpg")
-
-    for i in os.listdir("temp"):
-        st.write(i)
+    picture = cv2.imerad(image)
+    st.success("uplodaed")
     
 else:
     st.warning("Please upload a file for test")
